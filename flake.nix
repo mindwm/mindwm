8bd755c22542b1603b9aba4d943cebbfa1a0c6fa
Ryabin Sergey <ryabin.ss@gmail.com>
2024-04-06 13:12:43 +0200

    update deps

--- flake.nix
+++ flake.nix
@@ -2,12 +2,36 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.organist.url = "github:nickel-lang/organist";
+  inputs.argocd = {
+     type = "github";
+     owner = "argoproj";
+     repo = "argo-cd";
+     dir = "manifests/crds";
+     flake = false;
+  };
 
   nixConfig = {
     extra-substituters = ["https://organist.cachix.org" "https://nix-community.cachix.org"];
     extra-trusted-public-keys = ["organist.cachix.org-1:GB9gOx3rbGl7YEh6DwOscD1+E/Gc5ZCnzqwObNH2Faw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 
+
   outputs = {organist, ...} @ inputs:
     organist.flake.outputsFromNickel ./. inputs {};
+ in
+    {
+      templates.default = {
+        path = ./templates/default;
+        description = "A devshell using nickel.";
+        welcomeText = ''
+          You have just created an _Organist_-powered development shell.
+
+          - Enter the environment with `nix develop`
+          - Tweak it by modifying `project.ncl`
+
+          _Hint_: To be able to leverage the Nickel language server for instant feedback on your configuration, run `nix run .#regenerate-lockfile` first.
+        '';
+      };
+    };
+
 }
